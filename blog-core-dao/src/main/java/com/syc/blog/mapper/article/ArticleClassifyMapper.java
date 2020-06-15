1d43051d95c128a579ad94be80d72c2a543fa2da
sunyuchao <1481232419@qq.com>
2020-06-15 20:04:25 +0800

    文章分类重做

--- blog-core-dao/src/main/java/com/syc/blog/mapper/article/ArticleClassifyMapper.java
+++ blog-core-dao/src/main/java/com/syc/blog/mapper/article/ArticleClassifyMapper.java
@@ -14,9 +14,9 @@ public interface ArticleClassifyMapper extends BaseMapper<ArticleClassify> {
     @Select("select id,name from article_classify  where parent_id = 1  order by rand() limit #{num} ")
     List<ArticleClassify> selectRandomList(@Param("num") int i);
 
-    @Select("select u.*,(select count(*) from article_classify uu where uu.parent_id = u.id) as directChildrenCount from article_classify u  where u.level = #{level}")
+    @Select("select u.*,(select count(*) from article_classify uu where uu.parent_id = u.id and uu.archive = 0) as directChildrenCount from article_classify u  where u.level = #{level} and u.archive = 0")
     List<ArticleClassify> selectListByLevel(@Param("level") int i);
 
-    @Select("select u.*,(select count(*) from article_classify uu where uu.parent_id = u.id) as directChildrenCount from article_classify u  where u.parent_id = #{parentId}")
+    @Select("select u.*,(select count(*) from article_classify uu where uu.parent_id = u.id and uu.archive = 0) as directChildrenCount from article_classify u  where u.parent_id = #{parentId} and u.archive = 0")
     List<ArticleClassify> selectListByParentId(@Param("parentId") Integer id);
 }
