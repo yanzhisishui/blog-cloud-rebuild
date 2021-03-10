381402b09bc5157d8ec88be5cf83bd6d58b658b4
sunyuchao <yuchao.sun@hosjoy.com>
2021-03-10 15:06:05 +0800

    计算房贷还款

--- blog-core-onlineutils-service/src/main/java/com/syc/blog/controller/onlineutils/MortgageLoanController.java
+++ blog-core-onlineutils-service/src/main/java/com/syc/blog/controller/onlineutils/MortgageLoanController.java
@@ -1,15 +1,24 @@
 package com.syc.blog.controller.onlineutils;
 
+import com.alibaba.fastjson.JSON;
 import com.syc.blog.controller.BaseController;
+import com.syc.blog.response.MortgageInfo;
+import com.syc.blog.utils.ResultHelper;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 @RequestMapping("/util/mortgage/loan")
+@Slf4j
 public class MortgageLoanController extends BaseController {
 
 
@@ -19,60 +28,95 @@ public class MortgageLoanController extends BaseController {
         byte type = 16;
         getCurrentCommentsListPage(map,page,bindId,type);
         map.put("bindId",bindId);
-        return "onlineutils/mortagel_loan";
+        return "onlineutils/mortgage_loan";
     }
 
     /**
-     * 60 万贷款，贷款 30 年
-     * 公积金：3.25%，不用公积金：5.63%
-     * 等额本金
      * @param loanTotal 贷款总额
-     * @param year 贷款年限
+     * @param type 还款方式 1：等额本息 2：等额本金
      * @param annualRate 年利率
-     * @param type 还款方式 1：等额本金  2：等额本息
+     * @param year 还款分期（年）
      * */
-    public static void calcMortgage(BigDecimal loanTotal,BigDecimal annualRate,int year,int type){
+    @GetMapping("/calc")
+    @ResponseBody
+    public String calc(@RequestParam(value = "loanTotal",required = false,defaultValue = "600000") BigDecimal loanTotal,
+                       @RequestParam(value = "annualRate",required = false,defaultValue = "3.25") BigDecimal annualRate,
+                       @RequestParam(value = "year",required = false,defaultValue = "30") Integer year,
+                       @RequestParam(value = "type",required = false,defaultValue = "1") Integer type
+    ){
+       try {
+           year = year * 12; //期数
+           annualRate = annualRate.divide(new BigDecimal("100"),6,BigDecimal.ROUND_HALF_UP);
+           if(type == 1){
+               return calcMortgageEqualInterest(loanTotal,annualRate,year);
+           } else if(type ==2){
+               return calcMortgageEqualCapital(loanTotal,annualRate,year);
+           }
+       }catch (Exception e){
+           e.printStackTrace();
+           log.error("计算贷款信息失败",e);
+           return JSON.toJSONString(ResultHelper.wrapErrorResult(1,"计算异常"));
+       }
+       return null;
+    }
+
+    /**
+     * 等额本金
+     * */
+    private String calcMortgageEqualCapital(BigDecimal loanTotal, BigDecimal annualRate, Integer year) {
         BigDecimal totalInterestAmount = BigDecimal.ZERO;
         BigDecimal originLoanTotal = BigDecimal.valueOf(loanTotal.doubleValue());
-       if(type == 1){
-           BigDecimal capital = loanTotal.divide(new BigDecimal(30 * 12), 2, BigDecimal.ROUND_HALF_UP);//本金
-            for(int i=1;i<=year * 12;i++){
-                BigDecimal interest = loanTotal.multiply(annualRate).divide(new BigDecimal(12),2, BigDecimal.ROUND_HALF_UP);//利息
-                System.out.println("第 "+i+" 个月的本金："+capital);
-                System.out.println("第 "+i+" 个月的利息："+interest);
-                totalInterestAmount = totalInterestAmount.add(interest);
-                System.out.println("第 "+i+" 个月的月供："+ interest.add(capital));
-                System.out.println("-------------------------------");
-                loanTotal = loanTotal.subtract(capital);
-            }
-           System.out.println("等额本金总利息为："+totalInterestAmount);
-           System.out.println("还款总金额："+totalInterestAmount.add(originLoanTotal));
-       }
-       if(type == 2){
-           // 10000  ×  [4.5‰×(1+4.5‰) ^ 24]  ÷  {[(1+4.5‰) ^ 24]-1}=440.51元。
-           BigDecimal monthRate = annualRate.divide(new BigDecimal(12),10, BigDecimal.ROUND_HALF_UP);//月利率
-           System.out.println(monthRate);
-           double pow = Math.pow(new BigDecimal("1").add(monthRate).doubleValue(), year * 12);//（1+月利率）²
+        BigDecimal capital = loanTotal.divide(new BigDecimal(year), 2, BigDecimal.ROUND_HALF_UP);//本金
 
-           System.out.println(pow);
+        MortgageInfo info = new MortgageInfo();
+        List<MortgageInfo.MortgageItem> list = new ArrayList<>();
 
-           BigDecimal step2 = new BigDecimal(String.valueOf(pow)).subtract(new BigDecimal("1"));
-           System.out.println(step2);
+        for(int i=1;i<=year;i++){
+            BigDecimal interest = loanTotal.multiply(annualRate).divide(new BigDecimal(12),2, BigDecimal.ROUND_HALF_UP);//利息
+            MortgageInfo.MortgageItem item = new MortgageInfo.MortgageItem();
+            item.setIndex(i);
+            item.setCapital(capital);
+            item.setInterest(interest);
+            item.setMonthTotal(interest.add(capital));
+            totalInterestAmount = totalInterestAmount.add(interest);
+            loanTotal = loanTotal.subtract(capital);
+            list.add(item);
+        }
 
-           BigDecimal interest = loanTotal.multiply(monthRate).multiply(new BigDecimal(String.valueOf(pow))).divide(step2, 2, BigDecimal.ROUND_HALF_UP);//利息
-           for(int i= 1;i<=year * 12 ;i++){
-               BigDecimal lixi = loanTotal.multiply(annualRate).divide(new BigDecimal(12),2,BigDecimal.ROUND_HALF_UP);
-               System.out.println("第 "+i+" 个月利息："+lixi);
-               System.out.println("第 "+i+" 个月本金："+interest.subtract(lixi));
-               System.out.println("第 "+i+" 个月的月供："+interest);
-               System.out.println("-------------------------------");
-               loanTotal = loanTotal.subtract(interest.subtract(lixi));
-           }
-           System.out.println("还款总金额："+interest.multiply(new BigDecimal(year * 12)));
-       }
+        info.setInterestTotal(totalInterestAmount);
+        info.setItemList(list);
+        info.setTotalAmount(totalInterestAmount.add(originLoanTotal));
+        return JSON.toJSONString(ResultHelper.wrapSuccessfulResult(info));
     }
 
-    public static void main(String[] args) {
-        calcMortgage(new BigDecimal("600000"),new BigDecimal("0.0325"),30,2);
+    /**
+     * 等额本息
+     * */
+    private String calcMortgageEqualInterest(BigDecimal loanTotal, BigDecimal annualRate, Integer year) {
+        BigDecimal monthRate = annualRate.divide(new BigDecimal(12),10, BigDecimal.ROUND_HALF_UP);//月利率
+        double pow = Math.pow(new BigDecimal("1").add(monthRate).doubleValue(), year);//（1+月利率）²
+        BigDecimal step2 = new BigDecimal(String.valueOf(pow)).subtract(new BigDecimal("1"));
+        BigDecimal interestAndCapital = loanTotal.multiply(monthRate).multiply(new BigDecimal(String.valueOf(pow))).divide(step2, 2, BigDecimal.ROUND_HALF_UP);//利息
+        MortgageInfo info = new MortgageInfo();
+        List<MortgageInfo.MortgageItem> list = new ArrayList<>();
+
+        BigDecimal interestTotal = BigDecimal.ZERO;
+        for(int i= 1;i<=year ;i++){
+            BigDecimal interest = loanTotal.multiply(annualRate).divide(new BigDecimal(12),2,BigDecimal.ROUND_HALF_UP);
+            MortgageInfo.MortgageItem item = new MortgageInfo.MortgageItem();
+            item.setIndex(i);
+            item.setInterest(interest);
+            item.setCapital(interestAndCapital.subtract(interest));
+            item.setMonthTotal(interestAndCapital);
+            loanTotal = loanTotal.subtract(interestAndCapital.subtract(interest));
+            list.add(item);
+            interestTotal = interestTotal.add(interest);
+        }
+        info.setItemList(list);
+        info.setTotalAmount(interestAndCapital.multiply(new BigDecimal(year)));
+        info.setInterestTotal(interestTotal);
+        ResultHelper<MortgageInfo> result = ResultHelper.wrapSuccessfulResult(info);
+        return JSON.toJSONString(result);
     }
+
 }
